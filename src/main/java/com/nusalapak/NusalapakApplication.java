62b770124b52a0fa9f38771ad47f9fd153dd8193
IndarMuis <ismunandarmuis311@gmail.com>
2023-09-29 23:16:25 +0800

    initialized nusalapak project

---
+++ src/main/java/com/nusalapak/NusalapakApplication.java
@@ -0,0 +1,13 @@
+package com.nusalapak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NusalapakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NusalapakApplication.class, args);
+	}
+
+}
